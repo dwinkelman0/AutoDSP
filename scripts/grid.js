1d70d4cf6f26184e313980fca66b76b2bed13483
Daniel Winkelman <dwinkelman3@gmail.com>
2020-02-06 14:56:55 -0500

    Improved selection for nodes and edges

--- scripts/grid.js
+++ scripts/grid.js
@@ -1,12 +1,65 @@
+NODE_TYPES = {
+	"coordinate": ".",
+	"dspnodeopen": "Open Node",
+	"dspnodefull": "Full Node",
+	"dspadder": "Adder",
+	"dspmixer": "Mixer",
+	"dspsquare": "Square"
+};
+
+HOR_EDGE_TYPES = {
+	"none": " ",
+	"line": "Line",
+	"conn_right": "Right",
+	"conn_left": "Left",
+	"flow_right": "Flow Right",
+	"flow_left": "Flow Left"
+};
+
+VERT_EDGE_TYPES = {
+	"none": " ",
+	"line": "Line",
+	"conn_up": "Up",
+	"conn_down": "Down",
+	"flow_up": "Flow Up",
+	"flow_down": "Flow Down"
+};
+
+function newSelect(options)
+{
+	var select = $("<select/>");
+	for (var option in options)
+	{
+		var newOption = $("<option/>");
+		$(newOption).val(option);
+		$(newOption).text(options[option]);
+		$(select).append(newOption);
+	}
+	return select;
+}
+
 function newNode()
 {
-	var node = $("<td><select><option>Hello</option><option>World</option></select></td>");
+	var select = newSelect(NODE_TYPES);
+	var node = $("<td class='node'/>");
+	$(node).append(select);
+	$(node).css("background", "#90ee90");
 	return node;
 }
 
-function newEdge()
+function newHorEdge()
 {
-	var edge = $("<td><select><option>Right</option><option>Left</option></select></td>");
+	var select = newSelect(HOR_EDGE_TYPES);
+	var edge = $("<td class='hor_edge'/>");
+	$(edge).append(select);
+	return edge;
+}
+
+function newVertEdge()
+{
+	var select = newSelect(VERT_EDGE_TYPES);
+	var edge = $("<td class='vert_edge'/>");
+	$(edge).append(select);
 	return edge;
 }
 
@@ -20,7 +73,7 @@ function newNodeRow(size)
 	row = $("<tr/>");
 	for (var i = 0; i < size; i++)
 	{
-		$(row).append(i % 2 == 0 ? newNode() : newEdge());
+		$(row).append(i % 2 == 0 ? newNode() : newHorEdge());
 	}
 	return row;
 }
@@ -30,7 +83,7 @@ function newEdgeRow(size)
 	row = $("<tr/>");
 	for (var i = 0; i < size; i++)
 	{
-		$(row).append(i % 2 == 0 ? newEdge() : newBlank());
+		$(row).append(i % 2 == 0 ? newVertEdge() : newBlank());
 	}
 	return row;
 }
@@ -48,11 +101,11 @@ $(document).ready(function()
 		{
 			if (i % 2 == 0)
 			{
-				$(this).append([newEdge(), newNode()]);
+				$(this).append([newHorEdge(), newNode()]);
 			}
 			else
 			{
-				$(this).append([newBlank(), newEdge()]);
+				$(this).append([newBlank(), newVertEdge()]);
 			}
 			i++;
 		});
